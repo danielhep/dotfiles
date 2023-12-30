2ae44c94ba3e2da5049ce63f1bcd03a36b271238
Daniel Heppner <dh@danielheppner.com>
2023-12-30 00:20:06 -0800

    add alacritty

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -8,6 +8,7 @@
     ./fish.nix
     ./git.nix
     ./kitty.nix
+    ./alacritty.nix
     # ./emacs.nix
     # If you want to use home-manager modules from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModule

c7749fe5cf6f1c13bd76fe24f12bccad551c31f1
Daniel Heppner <danielhep@Daniels-MBP.localdomain>
2024-09-02 22:14:15 -0700

    add darwin folder

--- flake.nix
+++ flake.nix
@@ -9,6 +9,10 @@
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    # Nix-darwin
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+
     devenv.url = "github:cachix/devenv/latest";
     # TODO: Add any other flake you might need
     # hardware.url = "github:nixos/nixos-hardware";
@@ -19,7 +23,22 @@
     nix-colors.url = "github:misterio77/nix-colors";
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, darwin, ... }@inputs:
+  let
+    mkDarwinConfig = { system, username }: darwin.lib.darwinSystem {
+      inherit system;
+      specialArgs = { inherit inputs username; };
+      modules = [
+        ./darwin
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.${username} = import ./home-manager/macos.nix;
+        }
+      ];
+    };
+  in {
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
@@ -30,15 +49,24 @@
       };
     };
 
+    # Nix-darwin configuration entrypoint
+    # Available through 'darwin-rebuild switch --flake .#your-hostname'
+    darwinConfigurations = {
+      daniels-mbp = mkDarwinConfig {
+        system = "aarch64-darwin";
+        username = "danielhep";
+      };
+    };
+
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#configurationName'
     homeConfigurations = {
       "daniel.heppner@nse092852" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
-        modules = [ 
+        modules = [
           ./home-manager/macos.nix
-          { 
+          {
             home = {
               username = "daniel.heppner";
               homeDirectory = "/Users/daniel.heppner";
@@ -49,9 +77,9 @@
       "danielhep@daniels-mbp" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
-        modules = [ 
+        modules = [
           ./home-manager/macos.nix
-          { 
+          {
             home = {
               username = "danielhep";
               homeDirectory = "/Users/danielhep";
@@ -62,9 +90,9 @@
       "danielhep@personal-linux" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
-        modules = [ 
+        modules = [
           ./home-manager/linux.nix
-          { 
+          {
             home = {
               username = "danielhep";
               homeDirectory = "/home/danielhep";
--- setup.sh
+++ setup.sh
@@ -1,2 +1,12 @@
+#!/usr/bin/env bash
+
 LOWERCASE_HOSTNAME=$(hostname -s | awk '{print tolower($0)}')
-nix run home-manager -- --flake .#$(whoami)@$LOWERCASE_HOSTNAME switch
+OS=$(uname -s)
+
+if [ "$OS" = "Darwin" ]; then
+    echo "Detected macOS. Running nix-darwin..."
+    nix run nix-darwin -- switch --flake .#$LOWERCASE_HOSTNAME
+else
+    echo "Running home-manager..."
+    nix run home-manager -- --flake .#$(whoami)@$LOWERCASE_HOSTNAME switch
+fi

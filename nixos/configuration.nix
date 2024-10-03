90ac5981885ed91898ee5c61fe18fa5f368afb1f
danielhep <dh@danielheppner.com>
2024-10-02 22:39:57 -0700

    add hostname

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -62,7 +62,7 @@
   # FIXME: Add the rest of your current configuration
 
   # TODO: Set your hostname
-  networking.hostName = "your-hostname";
+  networking.hostName = "nixos-personal";
 
   # TODO: This is just an example, be sure to use whatever bootloader you prefer
   boot.loader.systemd-boot.enable = true;

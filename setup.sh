e2a5bea0134e6fdb965fbc29177d96462afc5a1b
danielhep <dh@danielheppner.com>
2024-10-02 22:25:47 -0700

    add nixos build confrig

--- setup.sh
+++ setup.sh
@@ -3,7 +3,10 @@
 LOWERCASE_HOSTNAME=$(hostname -s | awk '{print tolower($0)}')
 OS=$(uname -s)
 
-if [ "$OS" = "Darwin" ]; then
+if grep -q 'NAME=NixOS' /etc/os-release 2>/dev/null; then
+    echo "Detected NixOS. Rebuilding system configuration..."
+    sudo nixos-rebuild switch --flake .#$LOWERCASE_HOSTNAME
+elif [ "$OS" = "Darwin" ]; then
     echo "Detected macOS. Running nix-darwin..."
     nix run nix-darwin -- switch --flake .#$LOWERCASE_HOSTNAME
 else

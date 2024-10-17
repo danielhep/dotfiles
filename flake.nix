25d1a4e38262a7a3d86520ea132335101ded5d76
Daniel Heppner <daniel.heppner@ibigroup.com>
2024-10-17 15:35:26 -0700

    add work laptop

--- flake.nix
+++ flake.nix
@@ -71,6 +71,10 @@
           system = "aarch64-darwin";
           username = "danielhep";
         };
+        nse092852 = mkDarwinConfig {
+          system = "aarch64-darwin";
+          username = "daniel.heppner";
+        };
       };
 
       # Standalone home-manager configuration entrypoint
